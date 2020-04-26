8c6ae52eb3b4ebf25c80a9a8ff058639bf99207c
David Taylor <tayloredwebsites@me.com>
2020-04-26 11:11:01 -0400

    Added document model crud api
    - added model and controller
    - added mongo datasource and repository
    - works in REST API Explorer

---
+++ api/src/models/document.model.ts
@@ -0,0 +1,43 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Document extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+    description: 'Unique identifier of a document',
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+    description: 'Title of a document',
+  })
+  title: string;
+
+  @property({
+    type: 'string',
+    required: true,
+    description: 'Type of a document (e.g. editor)',
+  })
+  docType: string;
+
+  @property({
+    type: 'string',
+    description: 'Description of a document',
+  })
+  desc: string;
+
+
+  constructor(data?: Partial<Document>) {
+    super(data);
+  }
+}
+
+export interface DocumentRelations {
+  // describe navigational properties here
+}
+
+export type DocumentWithRelations = Document & DocumentRelations;
